9eba989f3889ceb62956a7133cb491568deeaaf2
Jessica Appelbaum <jessica.appelbaum@thinktopic.com>
2016-06-03 16:16:49 -0600

    created a middleware function that logs the time a request takes

--- src/zipcodes/handler.clj
+++ src/zipcodes/handler.clj
@@ -2,8 +2,8 @@
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
             [schema.core :as s]
-            [ring.swagger.schema :refer [coerce!]]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [zipcodes.core :refer [wrap-middleware]]))
 
 
 
@@ -18,7 +18,7 @@
                       :_id String
                       })
 
-(defonce data* (atom (take 4 zips-data)))
+(defonce data* (atom zips-data))
 
 (defn get-all-info [] (-> @data* reverse))
 
@@ -35,8 +35,8 @@
            :description "an exercize in getting started"}
     :tags [{:name "api", :description "this is a cool get thing that gets info about a zipcode, hopefully"}]}}
 
-  (context "/api" []
-           :tags ["api"]
+  (context "/zipcode" []
+           :tags ["zipcode"]
            
            (GET "/all" []
                 :return [ZipCode]
@@ -47,69 +47,6 @@
                 :return ZipCode
                 :path-params [id :- String]
                 :summary "retrieves info on a zipcode given a zipcode"
+                :middleware [wrap-middleware]
                 (ok (get-info id)))))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#_(def parsed-json
-    (coerce/coercer two coerce/json-coercion-matcher))
-
-;;(def two (take 2 zips-data))
-
-#_(defapi app
-  {:swagger
-   {:ui "/"
-    :spec "/swagger.json"
-    :data parsed-json}}
-
-  (context "/api" []
-           :tags ["api"]
-
-           (GET "/zip-info" []
-                :return (take 2 parsed-json))))
-
-;; (s/defschema Pizza
-;;   {:name s/Str
-;;    (s/optional-key :description) s/Str
-;;    :size (s/enum :L :M :S)
-;;    :origin {:country (s/enum :FI :PO)
-;;             :city s/Str}})
-
-;; (def app
-;;   (api
-;;     {:swagger
-;;      {:ui "/"
-;;       :spec "/swagger.json"
-;;       :data {:info {:title "Zipcodes"
-;;                     :description "Compojure Api example"}
-;;              :tags [{:name "api", :description "some apis"}]}}}
-
-;;     (context "/api" []
-;;       :tags ["api"]
-
-;;       (GET "/plus" []
-;;         :return {:result Long}
-;;         :query-params [x :- Long, y :- Long]
-;;         :summary "adds two numbers together"
-;;         (ok {:result (+ x y)}))
-
-;;       (POST "/echo" []
-;;         :return Pizza
-;;         :body [pizza Pizza]
-;;         :summary "echoes a Pizza"
-;;         (ok pizza)))))
