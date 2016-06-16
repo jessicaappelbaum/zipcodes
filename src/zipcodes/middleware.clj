9e7d242ae86401a5b873b04b3161eac8bab5cfe9
Jessica Appelbaum <jessica.appelbaum@thinktopic.com>
2016-06-16 09:23:19 -0600

    delete commented out pagination middleware

--- src/zipcodes/middleware.clj
+++ src/zipcodes/middleware.clj
@@ -26,31 +26,5 @@
   (fn [request]
     (let [response (handler request)
           response (assoc-in response [:headers "Access-Control-Allow-Origin"] "*")]
-      (clojure.pprint/pprint response)
+      ;;(clojure.pprint/pprint response)
       response)))
-
-
-;; this is the pagination middleware from think.datomic/src/clj/think/datomic/handler.clj
-
-;; (defn- get-param [req parameter-name]
-;;   (or (get-in req [:body-params parameter-name])
-;;       (get-in req [:params parameter-name])))
-
-;; (defn wrap-pagination
-;;   [handler]
-;;   (fn [request]
-;;     (let [response (handler request)
-;;           n        (get-param request :limit)
-;;           m        (get-param request :offset)]
-;;       (if (and (map? (:body response))
-;;                (contains? (:body response) :total-count)
-;;                (contains? (:body response) :items))
-;;         response
-;;         (if (and (boolean n)
-;;                  (boolean m)
-;;                  (boolean (seq (:body response))))
-;;           (assoc response
-;;                  :body {:total-count  (count (:body response))
-;;                         :items (take (Integer. n) (drop (Integer.  m) (:body response)))})
-;;           response)))))
-
